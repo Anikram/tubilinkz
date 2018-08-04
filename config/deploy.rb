5c53ef9d3a8d52afe7d557d552a1458186044b57
Anikram <4nikram@gmail.com>
2018-08-04 06:44:26 +0300

    capistrano ready

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,11 @@
 lock '3.8.1'
 
 set :application, 'tubilinkz'
-set :repo_url, 'git@github.com:Anikram/turbolinkz.git'
+set :repo_url, 'git@github.com:Anikram/tubilinkz.git'
 
-set :deploy_to, '/home/deploy/turbolinkz'
+set :deploy_to, '/home/apps/tubilinkz'
+
+set :passenger_restart_with_touch, false
 
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
